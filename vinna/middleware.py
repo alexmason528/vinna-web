f42b04a71698ed4be4fc1df8c4728ecce22e7d2f
Alex Mason <alexmason528@hotmail.com>
2017-08-25 14:51:53 -0400

    authorization middleware completed

--- vinna/middleware.py
+++ vinna/middleware.py
@@ -3,6 +3,11 @@ import base64
 from django.http import HttpResponse
 from django.conf import settings
 from django.core.urlresolvers import resolve
+from django.shortcuts import get_object_or_404
+from rest_framework_jwt.settings import api_settings
+from server.account.models import Account
+
+jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
 
 def _unauthed():
     response = HttpResponse('''
@@ -52,3 +57,40 @@ def disable_csrf_middleware(get_response):
         return get_response(request)
 
     return middleware
+
+def UserAuthMiddleware(get_response):
+    def middleware(request):
+        authorization = request.META.get('HTTP_AUTHORIZATION')
+        if not authorization:
+            return get_response(request)
+
+        headers = authorization.split(',')
+
+        if (len(headers) == 1):
+            return get_response(request)
+
+        (jwt_token, auth_param) = headers
+
+        jwt_token = jwt_token.replace('JWT ', '')
+        payload = jwt_decode_handler(jwt_token, request)
+
+        (name, value) = auth_param.split(' ')
+
+        if (name == 'account_id'):
+            account = None
+            try:
+                account = get_object_or_404(Account, id=value)
+            except:
+                response = HttpResponse('Unauthorized')
+                response.status_code = 401
+                return response
+
+            if account:
+                if account.user.id != payload['user_id']:
+                    response = HttpResponse('Unauthorized')
+                    response.status_code = 401
+                    return response
+
+        request.META['HTTP_AUTHORIZATION'] = headers[0]
+        return get_response(request)
+    return middleware 
